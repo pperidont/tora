7c0bf18fc1b970265cc5084f3cad4724941f12f9
Pascal Peridont <pperidont@motion-twin.com>
2013-08-19 22:59:20 +0200

    Add Queue.redisConnect() & WebSocket support

---
+++ src/RedisManager.hx
@@ -0,0 +1,257 @@
+class RedisManager {
+	
+	public var key : String;
+	public var host : String;
+	public var port : Int;
+
+	public var queues : Map<String, ModToraApi.Queue>;
+	public var lock : neko.vm.Mutex;
+
+	// Subscribe
+	var cnx : redis.Connection;
+	public var buffer : neko.vm.Deque<Array<String>>;
+	public var thread : neko.vm.Thread;
+	var connected : Bool;
+
+	// Publish
+	var pcnx : redis.Connection;
+	public var pbuffer : neko.vm.Deque<Array<String>>;
+	public var pthread : neko.vm.Thread;
+
+	public var closing : Bool;
+
+	public function new( key : String, host : String, port : Int ){
+		this.lock = new neko.vm.Mutex();
+		this.key = key;		
+		this.host = host;
+		this.port = port;
+
+		queues = new Map();
+		closing = false;
+		connected = false;
+
+		buffer = new neko.vm.Deque();
+		thread = neko.vm.Thread.create(subscribeLoop);
+
+		pbuffer = new neko.vm.Deque();
+		pthread = neko.vm.Thread.create(publishLoop);
+	}
+
+	function subscribeLoop(){
+		try {
+			var err = false;
+
+			try {
+				cnx = new redis.Connection(host,port);
+				cnx.setTimeout(1);
+				connected = true;
+				Tora.log("[Redis-Subscribe] Connected to "+key+".");
+			}catch(e : Dynamic ){
+			}
+
+			while( true ){
+				if( closing )
+					break;
+				var resp = null;
+				try {
+					if( cnx == null )
+						throw "Eof";
+					while( true ){
+						var a = buffer.pop(false);
+						if( a == null )
+							break;
+						cnx.command(a.shift(),a);
+					}
+					resp = cnx.receive();
+				}catch( e : Dynamic ){
+					switch( Std.string(e) ){
+						case "Blocked":
+						case "Eof","{}": // TODO why Eof become "{}" ??
+							try {
+								if( !err ){
+									err = true;
+									connected = false;
+									Tora.log("[Redis-Subscribe] "+key+" connection lost. Try to reconnect...");
+								}
+								cnx = new redis.Connection(host,port);
+								cnx.setTimeout(1);
+								var a = new Array();
+								lock.acquire();
+								for( q in queues )
+									a.push(q.name);
+								while( true ){
+									var a = buffer.pop(false);
+									if( a == null )
+										break;
+								}
+								lock.release();
+								cnx.command("SUBSCRIBE",a);
+								Tora.log("[Redis-Subscribe] "+key+" re-connected OK.");
+								connected = true;
+								err = false;
+							}catch( e : Dynamic ){
+								Sys.sleep(1);
+							}
+						default:
+							neko.Lib.rethrow(e);
+					}
+				}
+				if( resp != null ){
+					switch( resp ){
+						case R_Multi(a):
+							if( redis.Connection.str(a[0]) == "message" )
+								onMessage(redis.Connection.str(a[1]),redis.Connection.str(a[2]));
+						default: //
+					}
+				}
+			}
+
+			if( cnx != null ){
+				try{
+					cnx.close();
+				}catch( e : Dynamic ){
+				}
+				cnx = null;
+				buffer = null;
+				Tora.log("[Redis-Subscribe] "+key+" connection closed.");
+			}
+		}catch(e : Dynamic ){
+			Sys.print("Unrecoverable error in redis subscribeLoop: ");
+			Sys.println( e );
+			Sys.println( haxe.CallStack.toString(haxe.CallStack.exceptionStack()) );
+			neko.Lib.rethrow(e);
+		}
+	}
+
+	function publishLoop(){
+		try {
+			var err = false;
+
+			try {
+				pcnx = new redis.Connection(host,port);
+				pcnx.setTimeout(1);
+				Tora.log("[Redis-Publish] Connected to "+key+".");
+			}catch(e : Dynamic ){
+			}
+
+			while( true ){
+				if( closing )
+					break;
+				try {
+					var a = pbuffer.pop(true);
+					if( closing && a.length == 0 ){
+					}else if( pcnx == null ){
+						pbuffer.push(a);
+						throw "Eof";
+					}else if( !connected ){
+						pbuffer.push(a);
+						Sys.sleep(1);
+					}else{
+						try {
+							var b = Lambda.array(a);
+							pcnx.command(b.shift(),b);
+						}catch( e : Dynamic ){
+							pbuffer.push(a);
+							neko.Lib.rethrow(e);
+						}
+					}
+				}catch( e : Dynamic ){
+					switch( Std.string(e) ){
+						case "Blocked":
+						case "Eof","{}": // TODO why Eof become "{}" ??
+							try {
+								if( !err ){
+									err = true;
+									Tora.log("[Redis-Publish] "+key+" connection lost. Try to reconnect...");
+								}
+								pcnx = new redis.Connection(host,port);
+								pcnx.setTimeout(1);
+								Tora.log("[Redis-Publish] "+key+" re-connected OK.");
+								err = false;
+							}catch( e : Dynamic ){
+								pcnx = null;
+								Sys.sleep(1);
+							}
+						default:
+							neko.Lib.rethrow(e);
+					}
+				}
+			}
+
+			if( pcnx != null ){
+				try{
+					pcnx.close();
+				}catch( e : Dynamic ){
+				}
+				pcnx = null;
+				pbuffer = null;
+				Tora.log("[Redis-Publish] "+key+" connection closed.");
+			}
+		}catch(e : Dynamic ){
+			Sys.print("Unrecoverable error in redis publishLoop: ");
+			Sys.println( e );
+			Sys.println( haxe.CallStack.toString(haxe.CallStack.exceptionStack()) );
+			neko.Lib.rethrow(e);
+		}
+	}
+	
+	function onMessage( chan : String, msg : String ) {
+		lock.acquire();
+		if( closing )
+			return;
+		var q = queues.get(chan);
+		lock.release();
+
+		if( q == null )
+			return;
+		
+		var unserialize = false;
+		var msgData : Dynamic = null;
+
+		q.lock.acquire();
+		var ctx = new ModToraApi.ModuleContext();
+		for( qc in q.clients ) {
+			if( qc.c.needClose ) continue;
+			if( !ctx.initHandler(qc) )
+				qc.c.needClose = true;
+
+			if( !unserialize ){
+				msgData = try qc.h.unserialize( neko.NativeString.ofString(msg) ) catch( e : Dynamic ) null;
+				unserialize = true;
+			}
+			try {
+				qc.h.onNotify(msgData);
+			} catch( e : Dynamic ) {
+				var data = try {
+					var stack = haxe.CallStack.callStack().concat(haxe.CallStack.exceptionStack());
+					Std.string(e) + haxe.CallStack.toString(stack);
+				} catch( _ : Dynamic ) "???";
+				try {
+					qc.c.sendMessage(tora.Code.CError,data);
+				} catch( _ : Dynamic ) {
+					qc.c.needClose = true;
+				}
+			}
+			ctx.resetHandler(qc);
+			if( qc.c.needClose )
+				Tora.inst.close(qc.c, true);
+		}
+		ctx.restore();
+		q.lock.release();
+	}
+
+	public function removeQueue( q ){
+		buffer.add(["UNSUBSCRIBE",q.name]);
+		queues.remove( q.name );
+	}
+	
+	public function addQueue( q ){
+		queues.set( q.name, q );
+		buffer.add( ["SUBSCRIBE",q.name] );
+	}
+
+	public function close(){
+		closing = true;
+		pbuffer.add([]);
+	}
+}
---
+++ src/WSClient.hx
@@ -0,0 +1,319 @@
+import tora.Code;
+
+class WSClient extends Client {
+
+	static inline var GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+	var buf : haxe.io.Bytes;
+	var bufpos : Int;
+	var initialBufferSize : Int;
+	var maxBufferSize : Int;
+
+	var headersReceived : Bool;
+
+	public function new(s,secure){
+		super(s,secure);
+		initialBufferSize = (1 << 10);
+		maxBufferSize = (1 << 16);
+		bufpos = 0;
+		buf = haxe.io.Bytes.alloc(initialBufferSize);
+		headersReceived = false;
+		httpMethod = "WEBSOCKET";
+		protocol = "WEBSOCKET";
+	}
+
+	override public function prepare() {
+		dataBytes = 0;
+		outputHeaders = new List();
+		postData = null;
+		execute = null;
+	}
+
+
+	override function processMessage() : Bool{
+		if( cachedCode != null ){
+			var newbuf = haxe.io.Bytes.alloc(buf.length+1);
+			newbuf.set(0,cachedCode);
+			newbuf.blit(1,buf,0,buf.length);
+			buf = newbuf;
+			bufpos++;
+			cachedCode = null;
+		}
+
+		var available = buf.length - bufpos;
+
+		if( available == 0 ) {
+			var newsize = buf.length * 2;
+			if( newsize > maxBufferSize ) {
+				newsize = maxBufferSize;
+				if( buf.length == maxBufferSize )
+					throw "Max buffer size reached";
+			}
+			var newbuf = haxe.io.Bytes.alloc(newsize);
+			newbuf.blit(0,buf,0,bufpos);
+			buf = newbuf;
+			available = newsize - bufpos;
+		}
+
+		var bytes = sock.input.readBytes(buf,bufpos,available);
+		var pos = 0;
+		var len = bufpos + bytes;
+		while( len > 0 ) {
+			var m = read(pos,len);
+			if( m == 0 )
+				break;
+			len -= m;
+			pos += m;
+		}
+
+		if( pos > 0 )
+			buf.blit(0,buf,pos,len);
+		bufpos = len;
+
+		return execute;
+	}
+
+	function read( pos : Int, len : Int ) : Int {
+		if( !headersReceived ){
+			var p = pos;
+			while( p < pos+len ){
+				if( buf.get(p) == "\n".code )
+					break;
+				p++;
+			}
+			if( p >= pos+len )
+				return 0;
+			p -= pos;
+			var s = buf.readString(pos,p);
+			if( s.charCodeAt(s.length-1) == "\r".code )
+				s = s.substr(0,-1);
+			parseReq(s);
+			return p+1;
+		}else{
+			if( len < 6 )
+				return 0;
+			var p = pos;
+			var a = buf.get(p++);
+			var b = buf.get(p++);
+			var fin = a&128==128;
+			if( !fin )
+				throw "Unsupported";
+			var rsv = a&112;
+			var opcode = a&15;
+			var masked = (b&128==128);
+			if( !masked )
+				throw "Protocol error";
+			var l = (b&127);
+			if( l == 126 ){
+				if( len < 8 )
+					return null;
+				l = buf.get(p++)<<8 | buf.get(p++);
+			}else if( l == 127 )
+				throw "Unsupported data length";
+
+			var mask = new haxe.io.BytesInput(buf.sub(p,4)).readInt32();			
+			p += 4;
+
+			if( p-pos+l > len )
+				return null;
+			var data = buf.sub(p,l);
+
+			switch( opcode ){
+			case 1: // text
+				if( masked ){
+					postData = neko.Lib.stringReference(unmask(data,mask));
+					execute = true;
+				}
+			case 8: // close
+				send("",8);
+				execute = true;
+				needClose = true;
+
+			case 9: // ping
+				send(neko.Lib.stringReference(unmask(data,mask)),10);
+
+			case 10: // pong
+
+			default:
+				throw "Unsupported opcode";
+			}
+
+			
+			return p-pos+l;
+		}
+	}
+
+	function parseReq( s : String ){
+		if( s.length == 0 ){
+			hostName = getHeader("Host");
+			if( hostName == null )
+				throw "Missing host";
+
+			hostName = hostName.split(":")[0];
+
+			ip = sock.peer().host.toString();
+			file = Tora.inst.resolveHost(hostName);
+
+			headersReceived = true;
+			execute = true;
+			return;
+		}
+		var p = s.indexOf(": ");
+		if( p >= 0 ){
+			headers.push({k: s.substr(0,p), v: s.substr(p+2)});
+			return;
+		}
+
+		var f = s.indexOf(" ");
+		var l = s.lastIndexOf(" ");
+
+		uri = s.substr(f+1,l-f-1);
+		
+		var i = uri.indexOf("?");
+		if( i >= 0 ){
+			getParams = uri.substr(i+1);
+			parseGetParams();
+			uri = uri.substr(0,i);
+		}
+	}
+
+	function parseGetParams(){
+		var s = getParams;
+
+		while( true ){
+			var aand = s.indexOf("&");
+			var asep = s.indexOf(";");
+			if( aand == -1 || (asep != -1 && asep < aand) )
+				aand = asep;
+			
+			var p = aand==-1 ? s : s.substr(0,aand);
+			var aeq = p.indexOf("=");
+			if( aeq != -1 )
+				params.add({
+					k: StringTools.urlDecode(p.substr(0,aeq)), 
+					v: StringTools.urlDecode(p.substr(aeq+1))
+				});
+
+			if( aand == -1 )
+				break;
+
+			s = s.substr(aand+1);
+		}
+	}
+
+	function getHeader( k : String ){
+		k = k.toLowerCase();
+		for( h in headers )
+			if( h.k.toLowerCase() == k )
+				return h.v;
+		return null;
+	}
+
+	public override function sendHeaders() {
+		if( headersSent ) return;
+		headersSent = true;
+
+		sendHead("HTTP/1.1 101 Switching Protocols");
+		sendHead("Upgrade: websocket");
+		sendHead("Connection: Upgrade");
+		var key = getHeader("Sec-WebSocket-Key");
+		if( key == null )
+			throw "Missing Sec-WebSocket-Key header";
+
+		var accept = haxe.crypto.Sha1.encode(key+GUID);
+		accept = haxe.crypto.BaseCode.decode(accept,"0123456789abcdef");
+		accept = haxe.crypto.BaseCode.encode(accept,"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+
+		sendHead("Sec-WebSocket-Accept: "+accept+"=");
+
+		var k = null;
+		for( h in outputHeaders ){
+			switch( h.code ){
+			case CHeaderKey: k = h.str;
+			case CHeaderValue,CHeaderAddValue:
+				if( k != null )
+					sendHead(k+": "+h.str);
+			default:
+			}
+		}
+
+		sendHead("");
+	}
+
+	inline function sendHead( s : String ){
+		sendData(s+"\r\n");
+	}
+
+	function sendData(str){
+		var o = sock.output;
+		if( needClose ) return;
+		if( writeLock != null ) {
+			writeLock.acquire();
+			if( needClose ) {
+				writeLock.release();
+				return;
+			}
+			try {
+				o.writeString( str );
+			} catch( e : Dynamic ) {
+				writeLock.release();
+				neko.Lib.rethrow(e);
+			}
+			writeLock.release();
+		} else {
+			o.writeString( str );
+		}
+	}
+
+	function send( s : String, ?opcode = 1 ){
+		var d = new haxe.io.BytesOutput();
+		d.writeByte(128|(opcode&15));
+		var l = s.length;
+		if( l > 0xFFFF )
+			throw "Unsupported data length";
+		if( l > 125 ){
+			d.writeByte(126);
+			d.writeByte(l>>>8);
+			d.writeByte(l&0xFF);
+		}else{
+			d.writeByte(s.length);
+		}
+		if( s.length > 0 )
+			d.writeString(s);
+
+		sendData(neko.Lib.stringReference(d.getBytes()));
+	}
+
+	override function sendMessage( code : Code, msg : String ){
+		switch( code ){
+		case CPrint: send(msg);
+		case CError: 
+			send("Uncaught exception: "+msg);
+		case CListen:
+		default:
+			throw "Unexpected "+Std.string(code);
+		}
+	}
+
+	// ne pas allouer de buffer
+	static function unmask( data : haxe.io.Bytes, mask : Int ){
+		var r = new haxe.io.BytesOutput();
+		var p = 0;
+		
+		var l = data.length;
+		var f = 4-l%4;
+		if( f != 0 ){
+			var d = haxe.io.Bytes.alloc(l+f);
+			d.blit(0,data,0,l);
+			data = d;
+			l = data.length;
+		}
+		var bi = new haxe.io.BytesInput(data);
+		while( p < l ){
+			r.writeInt32( bi.readInt32() ^ mask );
+			p += 4;
+		}
+		return r.getBytes().sub(0,data.length-f);
+	}
+
+}
